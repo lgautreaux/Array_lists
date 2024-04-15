2588c6a23728d3dfcb068ac04f9ab0b0a2a638e7
lgautreaux <logangautreaux17@yahoo.com>
2024-04-14 19:10:58 -0700

    feat: add tail

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,14 +1,82 @@
 public class MyLinkedList<T> {
-    Node<T> head = null;
+    private Node<T> head = null, tail = null;
+    private int size = 0;
+
+    /**
+     * Getter for the start or head of the list
+     * @return
+     */
+    public Node<T> GetHead() {
+        return head;
+    }
+
+    /**
+     * Getter for the end or tail of the list
+     * @return
+     */
+    public Node<T> GetTail() {
+        return tail;
+    }
 
     public void AddFront(T data) {
         Node<T> newNode = new Node<T>(data);
         if (head == null) {
-            head = newNode;
+            head = tail = newNode;
         } else {
             newNode.next = head;
             head = newNode;
         }
+        size++;
+    }
+
+    /**
+     * Adds a new node to the end of the LinkedList
+     */
+    public void Append(T data) {
+
+        if (head == null) {
+            // Node<T> newNode = new Node<T>(data); // create the new node and ensure it's next is null
+            // size++;
+            // head = newNode;
+            AddFront(data);
+            return;
+        }
+
+        // Node<T> currentNode = head;
+
+        // while (currentNode.next != null) {
+        //     currentNode = currentNode.next;
+        // }
+
+        // once we have our current last node
+        Node<T> newNode = new Node<T>(data); // create the new node and ensure it's next is null
+        tail.next = newNode; // set the next of the old last node equal to the new node
+        tail = newNode;
+        size++;
+    }
+    
+    // 1
+    public void Insert(int index, T data) {
+
+        // we know they are asking to insert at the last element
+        if (index == size) {
+            Append(data);
+        }
+
+        if (index < 0 || index > size - 1) {
+            return;
+        }
+
+        Node<T> currentNode = head;
+
+        // go to the element prior to where we want to insert our element
+        for (int i = 0; i < index - 1; i++) {
+            currentNode = currentNode.next;
+        }
+        
+        Node<T> newNode = new Node<T>(data, currentNode.next);
+        currentNode.next = newNode;
+
     }
 
     @Override
@@ -17,10 +85,74 @@ public class MyLinkedList<T> {
         String returnString = "";
 
         while (currentNode != null) {
-            returnString += currentNode.data.toString();
+            returnString += " " + currentNode.data.toString();
             currentNode = currentNode.next;
         }
-        
+
         return returnString;
+    }
+
+    public void DeleteFront() {
+
+        if (head == tail) {
+            tail = null;
+        }
+
+        head = head.next;
+        size--;
+    }
+    
+    public void DeleteBack() {
+        Node<T> currentNode = head;
+
+        while (currentNode.next.next != null) {
+            currentNode = currentNode.next;
+        }
+        //System.out.println(currentNode.data);
+        currentNode.next = null;
+        tail = currentNode;
+        size--;
+    }
+
+    /**
+     * This will find the first instance of matching data and delete it
+     * @param data the data of type T whose node we want to delete
+     */
+    public void DeleteNodeBasedOnValue(T data) {
+        
+        if (head == null) {
+            return;
+        }
+        
+        Node<T> currentNode = head;
+
+        if (currentNode.data == data) {
+            DeleteFront();
+            return;
+        }
+
+        if (currentNode.next == null)
+            return;
+
+        while (currentNode.next.data != data) {
+            currentNode = currentNode.next;
+            if (currentNode.next == null)
+                return;
+        }
+        
+        //System.out.println(currentNode.data);
+        currentNode.next = currentNode.next.next;
+        if (currentNode.next == null) {
+            tail = currentNode;
+        }
+        size--;
+    }
+    
+    public int GetSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
     }
 }
--- Program.java
+++ Program.java
@@ -8,9 +8,29 @@ public class Program {
 
     public static void main(String[] args) {
         MyLinkedList<String> myLinkedList = new MyLinkedList<String>();
-        myLinkedList.AddFront("World!");
-        myLinkedList.AddFront("Hello");
+        myLinkedList.Append("World!");
+        // Add a new Node here
+        myLinkedList.Append("Hello");
+        myLinkedList.Append("This");
+        myLinkedList.Append("Hi");
+        myLinkedList.Append("Dog");
+        System.out.println(myLinkedList.GetSize());
         System.out.println(myLinkedList);
+        myLinkedList.Insert(1, "Leaf");
+        System.out.println(myLinkedList);
+        myLinkedList.DeleteFront();
+        System.out.println(myLinkedList);
+        myLinkedList.Append("Cat");
+        System.out.println(myLinkedList);
+        myLinkedList.AddFront("Gato");
+        System.out.println(myLinkedList);
+        myLinkedList.DeleteBack();
+        System.out.println(myLinkedList);
+        myLinkedList.DeleteNodeBasedOnValue("Hello");
+        System.out.println(myLinkedList);
+        System.out.println("Head" + myLinkedList.GetHead().data);
+        System.out.println("Tail" + myLinkedList.GetTail().data);
+
     }
     
 }
